c1fce8594d4162d82d332c46006460399947f150
nepptune-studio <nunu@nepptune.studio>
2024-07-19 05:50:13 +0900

    덤핑

---
+++ quilljs/post-editor.js
@@ -0,0 +1,13 @@
+<script src="https://cdn.jsdelivr.net/npm/quill@2/dist/quill.js"></script>
+// Add fonts to whitelist
+const Font = Quill.import('formats/font');
+// We do not add Aref Ruqaa since it is the default
+Font.whitelist = ['mirza', 'roboto'];
+Quill.register(Font, true);
+
+const quill = new Quill('#editor', {
+  modules: {
+    toolbar: '#toolbar',
+  },
+  theme: 'snow',
+});
